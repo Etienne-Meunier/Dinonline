70c5817037c9994e142a17243467bedf9cfaf199
Etienne Meunier <etiennemeunier@live.fr>
2025-08-08 19:41:33 +0200

    setup links

--- restart_0/scripts/setup_links.sh
+++ restart_0/scripts/setup_links.sh
@@ -3,11 +3,18 @@ REBUILD_NEMO=/lustre/fswork/projects/rech/omr/uym68qx/nemo_4.2.1/tools/REBUILD_N
 RESTART_FILES=/lustre/fsn1/projects/rech/omr/romr004/data/restart_files/
 ZB_DINO=/lustre/fsn1/projects/rech/omr/romr004/code/ZB_DINO/
 
-echo "Setup symlinks"
+echo "Setup symlinks:"
 
+echo "                nemo -> $NEMO_EXECUTABLE"
 ln -sfn $NEMO_EXECUTABLE nemo
+
+echo "                rebuild_nemo.exe -> $REBUILD_NEMO"
 ln -sfn $REBUILD_NEMO rebuild_nemo.exe
+
+echo "                restart_files -> $RESTART_FILES"
 ln -sfn $RESTART_FILES restart_files
+
+echo "                ZB_DINO -> $ZB_DINO"
 ln -sfn $ZB_DINO ZB_DINO
 
 echo "Done"
